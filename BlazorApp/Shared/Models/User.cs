2c3eea46f04a2ea96ad03de95196cf3babceb715
Naveen <Naveen@LAPTOP-NVB8GCAB>
2024-06-18 11:13:08 +0530

    Add project files.

---
+++ BlazorApp/Shared/Models/User.cs
@@ -0,0 +1,14 @@
+﻿#nullable disable
+
+
+namespace BlazorApp.Shared.Models
+{
+    public class User
+    {
+        public int Userid { get; set; }
+        public string Username { get; set; }
+        public string Address { get; set; }
+        public string Cellnumber { get; set; }
+        public string Emailid { get; set; }
+    }
+}
